4a04f194da6a3e55183ea94b0962641cbf0eac7c
monhtulgae <m.elbegsaikhan01@gmail.com>
2025-10-08 22:47:29 +0800

    OSF

--- hadgal-miniapp/app/api/organizations/[id]/route.ts
+++ hadgal-miniapp/app/api/organizations/[id]/route.ts
@@ -1,52 +1,26 @@
-// import { NextResponse } from "next/server";
-// import fs from "fs/promises";
-// import path from "path";
-
-
-// interface Params {
-//   params: { id: string };
-// }
-
-// export async function GET(request: Request, { params }: Params) {
-//   const { id } = params;
-
-//   try {
-//     const filePath = path.join(process.cwd(), "data", "organizations.json");
-//     const fileData = await fs.readFile(filePath, "utf-8");
-//     const organizations = JSON.parse(fileData);
-
-//     return NextResponse.json(organizations[Number(id) - 1]);
-//   } catch (error) {
-//     console.error("Error reading organizations.json:", error);
-//     return NextResponse.json(
-//       { error: "Failed to load organizations" },
-//       { status: 500 }
-//     );
-//   }
-// }
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import fs from "fs/promises";
 import path from "path";
 
-export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } | Promise<{ id: string }> }
-) {
-  // Await params to cover both local and Vercel build
-  const params = await context.params;
-  const { id } = params;
+
+interface Params {
+  params: { id: string };
+}
+
+export async function GET(request: NextRequest, context: Promise<{ params: { id: string } }>) {
+  const { params } = await context;
+  const id = await params.id;
 
   try {
-    const filePath = path.join(process.cwd(), "data", "projects.json");
+    const filePath = path.join(process.cwd(), "data", "organizations.json");
     const fileData = await fs.readFile(filePath, "utf-8");
-    const projects = JSON.parse(fileData);
+    const organizations = JSON.parse(fileData);
 
-    // Return project by ID
-    return NextResponse.json(projects[Number(id) - 1]);
+    return NextResponse.json(organizations[Number(id) - 1]);
   } catch (error) {
-    console.error("Error reading projects.json:", error);
+    console.error("Error reading organizations.json:", error);
     return NextResponse.json(
-      { error: "Failed to load project" },
+      { error: "Failed to load organizations" },
       { status: 500 }
     );
   }
--- hadgal-miniapp/app/organizations/[id]/page.tsx
+++ hadgal-miniapp/app/organizations/[id]/page.tsx
@@ -1,11 +1,12 @@
 import OrganizationDetail from "@/app/components/OrgDetail";
+import { use } from 'react'
 
 interface PageProps {
   params: { id: string };
 }
 
-export default function Page({ params }: PageProps) {
-  const id = params.id;
+export default async function Page({ params }: PageProps) {
+  const id = await params.id;
 
   return <OrganizationDetail orgId={id} />;
 }
