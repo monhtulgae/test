450f455d11bd83c9efc356c2ea0e875d8e5c2dc0
Bat-BBE <baterdenebatbayr321@gmail.com>
2025-10-08 21:57:00 +0800

    new

--- hadgal-miniapp/app/organizations/[id]/page.tsx
+++ hadgal-miniapp/app/organizations/[id]/page.tsx
@@ -1,11 +1,24 @@
-import OrganizationDetail from "@/app/components/OrgDetail";
+import { NextResponse, NextRequest } from "next/server";
+import fs from "fs/promises";
+import path from "path";
 
-interface PageProps {
-  params: { id: string };
-}
-
-export default async function Page({ params }: PageProps) {
+export async function GET(
+  request: NextRequest, 
+  { params }: { params: { id: string } }
+) {
   const id = params.id;
 
-  return <OrganizationDetail orgId={id} />;
+  try {
+    const filePath = path.join(process.cwd(), "data", "organizations.json");
+    const fileData = await fs.readFile(filePath, "utf-8");
+    const organizations = JSON.parse(fileData);
+
+    return NextResponse.json(organizations[Number(id) - 1]);
+  } catch (error) {
+    console.error("Error reading organizations.json:", error);
+    return NextResponse.json(
+      { error: "Failed to load organizations" },
+      { status: 500 }
+    );
+  }
 }
